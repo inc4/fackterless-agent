c5c330fc246c3962cac8cc31205f83c12df95785
Oleksiy Pyltsov <valar999@gmail.com>
2024-07-07 10:30:01 +0200

    Add a test

--- test/Agent.ts
+++ test/Agent.ts
@@ -19,18 +19,51 @@ describe("Agent", function () {
     }
 
     it("Should process tweets", async () => {
-        const {agent, oracle, storage, owner} = await loadFixture(deploy);
+        const { agent, oracle, storage, owner } = await loadFixture(deploy);
+        const login = "VitalikButerin";
 
-	await storage.createUser("295218901", "VitalikButerin");
-	await storage.addTweet("295218901", "1729251834404249696", "2023-11-27T21:32:19.000Z", "I criticize parts of *both* the e/acc and EA camps for being too willing to put their trust in a single centralized actor, whether a nonprofit or a national government, in their solutions. https://t.co/rwalZlGSGv");
-	await storage.addTweet("295218901", "1729251838581727232", "2023-11-27T21:32:20.000Z", "My philosophy: d/acc https://t.co/GDzrNrmQdz");
-        const tx = await agent.runAgent("VitalikButerin");
+        await storage.createUser("295218901", login);
+
+        await storage.addTweet("295218901", "1729251834404249696", "2023-11-27T21:32:19.000Z", "I criticize parts of *both* the e/acc and EA camps for being too willing to put their trust in a single centralized actor, whether a nonprofit or a national government, in their solutions. https://t.co/rwalZlGSGv");
+        await storage.addTweet("295218901", "1729251838581727232", "2023-11-27T21:32:20.000Z", "My philosophy: d/acc https://t.co/GDzrNrmQdz");
+
+        let iteration = 0;
+
+        const tx = await agent.runAgent(login);
         const res = await tx.wait();
+        const id = res.logs[1].args[0];
 
-	await oracle.addOpenAiResponse(0, 0, ['chatcmpl-9iEtr2rQUDJTHmgknueM6LCbpGmgE', 'Hello! How can I assist you today?', '', '', 1720330735, 'gpt-4-0125-preview', '', 'chat.completion', 10, 87, 97], '');
 
-	console.log(await agent.getAgentRun(0));
-	// const user = await storage.getUser("295218901");
-	// expect(user).to.equal("VitalikButerin");
+        // Step 1
+        let run = await agent.getAgentRun(id);
+        expect(run.iteration).to.equal(iteration + 1);
+        expect(run.isFinished).to.equal(true);
+        await oracle.addOpenAiResponse(id, iteration, ['chatcmpl-9iEtr2rQUDJTHmgknueM6LCbpGmgE', '{"about_bitcoin": "true","is_prediction": "true","up": "true"}', '', '', 1720330735, 'gpt-4-0125-preview', '', 'chat.completion', 10, 87, 97], '');
+        // await oracle.addScriptResponse(id, iteration, '{"isCorrect": "true"}', '');
+
+        let user = await storage.getUserByLogin(login);
+        let tweets = await agent.user.tweets(user.id);
+        expect(tweets[iteration].userId).to.equal(user.id);
+        expect(tweets[iteration].tweetId).to.equal("1729251834404249696");
+        expect(tweets[iteration].isCorrect).to.equal(true);
+        iteration++;
+
+        // Step 2
+        run = await agent.getAgentRun(id);
+        expect(run.iteration).to.equal(iteration + 1);
+        expect(run.isFinished).to.equal(true);
+
+        await oracle.addOpenAiResponse(id, iteration, ['chatcmpl-9iEtr2rQUDJTHmgknueM6LCbpGmgE', '{"about_bitcoin": "true","is_prediction": "true","up": "true"}', '', '', 1720330735, 'gpt-4-0125-preview', '', 'chat.completion', 10, 87, 97], '');
+        // await oracle.addScriptResponse(id, iteration, '{"isCorrect": "true"}', '');
+
+        user = await storage.getUserByLogin(login);
+        tweets = await agent.user.tweets(user.id);
+        expect(tweets[iteration].userId).to.equal(user.id);
+        expect(tweets[iteration].tweetId).to.equal("1729251834404249696");
+        expect(tweets[iteration].isCorrect).to.equal(true);
+        iteration++;
+
+
     });
+
 });
